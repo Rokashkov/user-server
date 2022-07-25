68466b77647a19f3d9893fddb55c5a85144cc830
Vladimir <vovalol31@gmail.com>
2022-07-25 10:38:14 +0300

    scripts

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,12 @@ import errorMiddleware from './middlewares/errorMiddleware'
 
 dotEnv.config()
 
+if (process.env.NODE_ENV === 'production') {
+	process.env.DATABASE_URL = 'postgresql://vladimir:89235275785Lol!@localhost:5432/app'
+	process.env.API_DOMAIN = 'http://45.147.178.215:5000/api'
+	process.env.CLIENT_DOMAIN = 'http://45.147.178.215:8080'
+}
+
 const PORT = process.env.PORT
 
 const app = express()
@@ -19,5 +25,6 @@ app.use('/api', router)
 app.use(errorMiddleware)
 
 app.listen(PORT, (): void => {
-	console.log(`Server has been started on PORT: ${ PORT }...`)
+	console.log(`Server has been started on PORT: ${ PORT } in ${ process.env.NODE_ENV } mode...`)
+	console.log(process.env.API_DOMAIN)
 })
